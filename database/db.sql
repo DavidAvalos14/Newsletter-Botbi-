6c7a89a7d41b387951fc62c7736b410739c13830
DavidAvalos14 <angeldavidavaloscarrillo@gmail.com>
2026-01-17 18:21:52 -0600

    Database script and diagram

---
+++ database/db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "suscriptores" (
+    "id_subs" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "subs_email" VARCHAR(255) UNIQUE NOT NULL,
+    "subs_password" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "mercados" (
+    "id_mercados" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "mcds_nombre" VARCHAR(255) UNIQUE NOT NULL,
+    "mcds_precio" DECIMAL(12, 2) NOT NULL,
+    "mcds_tipo" VARCHAR(20) NOT NULL CHECK ("mcds_tipo" IN ('Cripto', 'Accion'))
+);
+
+CREATE TABLE "noticias" (
+    "news_uuid" UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- Autogenerado
+    "news_titulo" VARCHAR(255) UNIQUE NOT NULL,
+    "news_contenido" TEXT NOT NULL,
+    "news_fecha" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    "news_categoria" VARCHAR(50) NOT NULL CHECK ("news_categoria" IN ('Tecnologia', 'Negocios')),
+    "news_url" TEXT UNIQUE NOT NULL 
+);
