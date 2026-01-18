99a6716ec7caf55287f3b6b6e603bacf3de56a98
DavidAvalos14 <angeldavidavaloscarrillo@gmail.com>
2026-01-17 20:18:44 -0600

    Image field in cripto and actions

--- database/db.sql
+++ database/db.sql
@@ -8,6 +8,7 @@ CREATE TABLE "mercados" (
     "id_mercados" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "mcds_nombre" VARCHAR(255) UNIQUE NOT NULL,
     "mcds_precio" DECIMAL(12, 2) NOT NULL,
+    "mcds_imagen" TEXT NOT NULL,
     "mcds_tipo" VARCHAR(20) NOT NULL CHECK ("mcds_tipo" IN ('Cripto', 'Accion'))
 );
 
